e4c407e3479581e8fb4191762e08dfc2fc615018
Andrew McGill <arm5077@gmail.com>
2022-02-12 22:48:49 -0500

    Fix prod issue

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,9 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'react-tween-titan.production.js',
   },
-};
+});
